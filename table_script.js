b4372da702d0353dac491a757746fa3a98f994c0
Vishwajit Khanna <47598526+vishwajit247@users.noreply.github.com>
2023-02-18 18:32:43 +0530

    Update table_script.js

--- table_script.js
+++ table_script.js
@@ -1,6 +1,6 @@
 function edit_row(a)
 {
- document.getElementById("edit_button"+a).style.display="ane";
+ document.getElementById("edit_button"+a).style.display="none";
  document.getElementById("save_button"+a).style.display="block";
 	
  var name=document.getElementById("name_row"+a);
@@ -49,3 +49,41 @@ function add_row()
  document.getElementById("new_roll").value="";
  document.getElementById("new_address").value="";
 }
+
+(function(document) {
+    'use strict';
+
+    var TableFilter = (function(myArray) {
+        var search_input;
+
+        function _onInputSearch(e) {
+            search_input = e.target;
+            var tables = document.getElementsByClassName(search_input.getAttribute('data-table'));
+            myArray.forEach.call(tables, function(table) {
+                myArray.forEach.call(table.tBodies, function(tbody) {
+                    myArray.forEach.call(tbody.rows, function(row) {
+                        var text_content = row.textContent.toLowerCase();
+                        var search_val = search_input.value.toLowerCase();
+                        row.style.display = text_content.indexOf(search_val) > -1 ? '' : 'none';
+                    });
+                });
+            });
+        }
+
+        return {
+            init: function() {
+                var inputs = document.getElementsByClassName('search-input');
+                myArray.forEach.call(inputs, function(input) {
+                    input.oninput = _onInputSearch;
+                });
+            }
+        };
+    })(Array.prototype);
+
+    document.addEventListener('readystatechange', function() {
+        if (document.readyState === 'complete') {
+            TableFilter.init();
+        }
+    });
+
+})(document);
